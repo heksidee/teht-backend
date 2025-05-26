00e9c76055401c576f31fb1417a8b150bb9ee475
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-26 14:36:33 +0300

    luotu mongodb tietokanta

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.y9hmhv7.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -20,11 +20,18 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is easy',
+  content: 'Mongoose makse things easy',
   important: true,
 })
 
-note.save().then(result => {
+/*note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
+})*/
+
+Note.find({ important: false }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
 })
