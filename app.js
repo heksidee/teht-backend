cabe3dc7d45f1fcb7bbcb7a48fe910190deeea61
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-06-04 11:33:55 +0300

    korjattu typo

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use("./api/notes", notesRouter)
+app.use("/api/notes", notesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const app = require("./app")
 const config = require("./utils/config")
 const logger = require("./utils/logger")
 
-const cors = require('cors')
-app.use(cors())
-
 app.listen(config.PORT, () => {
   logger.info(`Server is running on port ${config.PORT}`)
 })
